e4790f5805dc1a05306c0c8ea890cec77887a0a3
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-21 20:42:36 +0700

    ex4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+
+    double a[10];
+    double b[10];
+
+    int x = atoi(argv[1]);
+    int k = atoi(argv[2]);
+
+    for(int i = 0; i < 10; i++){
+        scanf("%lf",&a[i]);
+    }
+    for(int i = 0; i < 10; i++){
+        if(i < k){b[i] = a[i];}
+        else if(i == k){b[i] = x;}
+        else{b[i] = a[i - 1];}
+    }
+
+    for(int i = 0; i < 10; i++){printf("%.3f ", b[i]);}
+
     return 0;
 }

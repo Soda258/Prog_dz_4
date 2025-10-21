722935f13fa8c6e9386b62c0ee794f26347720dc
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-21 20:03:45 +0700

    ex3

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    
+    double a[10];
+    double b[10];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%lf",&a[i]);
+    }
+    int k = atoi(argv[1]);
+    int j = k % 10;
+    if(k > -1){
+        for(int i = 0; i < 10; i++){
+            if(j<10){b[i]=a[i];}
+            else{b[i%10] = a[i];}
+        j++;
+        }
+    }
+    else{
+        int l = 10 + j;
+        for(int i = 0; i < 10; i++){
+            if(l < 10){b[l] = a[i];}
+            else{b[l%10]=a[i];}
+        }
+        l++;
+    }
+    for(int i = 0; i < 10; i++){printf("%.3f ", b[i]);}
+
+
     return 0;
 }

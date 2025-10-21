cdbbaadd6d10ce22db5685530d75115475ec6e58
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-21 19:09:17 +0700

    all

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+
+    double sum = 0;
+    for(int i=1;i<=10;i++){sum += i;}
+    printf("%f",sum/10);
+
     return 0;
 }

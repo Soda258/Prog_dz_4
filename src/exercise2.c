a50814cedefb131de7a86c43a6de7ef455f5ec9e
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-21 19:55:47 +0700

    ex2

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+
+    double a[10];
+    for(int i = 0; i < 10; i++){
+        scanf("%lf", &a[i]);
+    }
+    for(int i = 10; i > 0; i--){
+        printf("%.3f ",a[i-1]);
+    }
+
     return 0;
 }

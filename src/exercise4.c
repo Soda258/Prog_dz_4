eee11f772d795e5b3484c315537552d8a730b6db
Семёнов Дмитрий Юрьевич <semenovdmitry28@gmail.com>
2025-10-25 17:43:28 +0700

    ex

--- src/exercise4.c
+++ src/exercise4.c
@@ -9,16 +9,18 @@ int main(int argc, char** argv) {
     int x = atoi(argv[1]);
     int k = atoi(argv[2]);
 
-    for(int i = 0; i < 10; i++){
-        scanf("%lf",&a[i]);
-    }
-    for(int i = 0; i < 10; i++){
-        if(i < k){b[i] = a[i];}
-        else if(i == k){b[i] = x;}
-        else{b[i] = a[i - 1];}
+    for (int i = 0; i < 10; i++) {scanf("%lf", &a[i]);}
+
+    for (int i = 0; i < 10; i++) {
+        if (i < k) {b[i] = a[i];}
+        else if (i == k) {b[i] = x;}
+        else {b[i] = a[i - 1];}
     }
 
-    for(int i = 0; i < 10; i++){printf("%.3f ", b[i]);}
+    for (int i = 0; i < 10; i++) {
+        if (i == 10 - 1) {printf("%.3f", b[i]);}
+        else {printf("%.3f ", b[i]);}
+    }
 
     return 0;
 }
--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+
+    double mA[10][3];
+    double mB[3][10];
+    double result[10][10];
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 3; j++) {scanf("%lf", &mA[i][j]);}
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++) {scanf("%lf", &mB[i][j]);}
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < 3; k++) {result[i][j] += mA[i][k] * mB[k][j];}
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%lf", result[i][j]);
+            if (i != 10 - 1 || j != 10 - 1) {printf(" ");}
+        }
+    }
+
     return 0;
 }
